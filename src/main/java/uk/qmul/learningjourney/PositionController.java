bc4a72ba3c4ac999e5258bd7667894b0b886424b
c.shi <129512850+Chenx-shi@users.noreply.github.com>
2023-04-19 19:03:41 +0800

    Add files via upload

---
+++ src/main/java/uk/qmul/learningjourney/PositionController.java
@@ -0,0 +1,24 @@
+package uk.qmul.learningjourney;
+
+import javafx.fxml.FXML;
+import java.io.IOException;
+
+public class PositionController {
+
+    @FXML
+    private void switchToContest() throws IOException {
+        //showcontest
+    }
+    @FXML
+    private void switchToPosition() throws IOException {
+        //showposition
+    }
+    @FXML
+    private void switchToVolunteer() throws IOException {
+        //showvolunteer
+    }
+    @FXML
+    private void switchToHonor() throws IOException {
+        //showhonor
+    }
+}
