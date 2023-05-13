e4e28e0af66f01eedd00f8113ca0220ef3c961e2
sager404 <71086060+sager404@users.noreply.github.com>
2023-05-13 22:06:01 +0800

    Improve schedule-view and add course-selection-view

--- src/main/java/uk/qmul/learningjourney/CourseSelectionController.java
+++ src/main/java/uk/qmul/learningjourney/CourseSelectionController.java
@@ -1,4 +1,35 @@
 package uk.qmul.learningjourney;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
+
+import java.util.ArrayList;
+
 public class CourseSelectionController extends BaseController {
+    @FXML
+    private GridPane gridPane;
+    private ArrayList<RowConstraints> rowConstraints;
+
+    public void addRow() {
+        RowConstraints row = new RowConstraints();
+        gridPane.addRow(gridPane.getRowCount());
+
+    }
+
+    public void addLabel() {
+
+    }
+
+    public void addButton(int row) {
+        Button button = new Button("Select");
+        gridPane.add(button, row, 4);
+        button.setOnAction(actionEvent -> {
+            button.getStyleClass().add("success");
+            button.setText("Selected!");
+            button.setDisable(true);
+        });
+    }
+
 }
