665213ca952164bac3813f737b1ffaf545b60ab8
harryharrah <m13241846062@163.com>
2023-05-14 01:32:38 +0800

    add Grade and Achievement

---
+++ src/main/java/uk/qmul/learningjourney/Achievenment.java
@@ -0,0 +1,14 @@
+package uk.qmul.learningjourney;
+
+public class Achievenment {
+    public String name;
+    public String date;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
---
+++ src/main/java/uk/qmul/learningjourney/Grade.java
@@ -0,0 +1,19 @@
+package uk.qmul.learningjourney;
+
+public class Grade {
+    public String name;
+    public String credit;
+    public String score;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCredit(String credit) {
+        this.credit = credit;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+}
