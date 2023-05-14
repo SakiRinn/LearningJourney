df0c2794dcfed32b95510c15fe17486e320d1ea1
sager404 <71086060+sager404@users.noreply.github.com>
2023-05-14 13:21:34 +0800

    Courses and students import.

--- src/test/java/uk/qmul/learningjourney/DataIOTest.java
+++ src/test/java/uk/qmul/learningjourney/DataIOTest.java
@@ -35,9 +35,22 @@ class DataIOTest {
                 "3-535", 6, new int[]{1, 4, 9, 13}, new int[]{1, 2, 3, 4}, new int[]{10, 11});
         ArrayList<Course> courses = new ArrayList<>();
         courses.add(course1);
-        Student student1 = new Student("2020213181", "Wu lyuhua", "123456",
+        Student student1 = new Student("2020213171", "Wu lyuhua", "123456",
                 "International School", "Telecommunication and management", "2020215105", courses);
+        Course course2 = new Course("PDP", "EBC5001", "Mona JABER", 0, 0.5,
+                "3-208", 6, new int[]{9}, new int[]{2}, new int[]{8, 9});
+        courses.add(course2);
+        Student student2 = new Student("2020213160", "Huang Xiyuan", "123456",
+                "International School", "Telecommunication and management", "2020215105", courses);
+        Course course3 = new Course("Image and Video Processing", "EBU6230", "Gokop Goteng", 0, 3.0,
+                "3-535", 6, null, new int[]{5}, new int[]{1, 2});
+        courses.add(course3);
+        Student student3 = new Student("2020213156", "Liu Zekai", "123456",
+                "International School", "Telecommunication and management", "2020215105", courses);
+
         DataIO.saveObject(student1);
+        DataIO.saveObject(student2);
+        DataIO.saveObject(student3);
         System.out.println(student1.getClass());
     }
 
@@ -80,11 +93,14 @@ class DataIOTest {
                 "3-535", 6, new int[]{1, 4, 9, 13}, new int[]{1, 2, 3, 4}, new int[]{10, 11});
         Course course2 = new Course("PDP", "EBC5001", "Mona JABER", 0, 0.5,
                 "3-208", 6, new int[]{9}, new int[]{2}, new int[]{8, 9});
-        Course course3 = new Course("Image and Videorocessing", "EBU6230", "Gokop Goteng", 0, 3.0,
+        Course course3 = new Course("Image and Video Processing", "EBU6230", "Gokop Goteng", 0, 3.0,
                 "3-535", 6, null, new int[]{5}, new int[]{1, 2});
+        Course course4 = new Course("Internet Application", "BBU1234", "Huang Xiaohong", 0, 3.0,
+                "3-535", 6, null, new int[]{1}, new int[]{3, 4});
         DataIO.saveObject(course1);
         DataIO.saveObject(course2);
         DataIO.saveObject(course3);
+        DataIO.saveObject(course4);
     }
 
 
